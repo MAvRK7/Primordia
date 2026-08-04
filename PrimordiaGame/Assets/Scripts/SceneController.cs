9cb1801aaa188158724b14b6c4ec2e6f4c3950bd
DestroyerMob <ethanhellyer7@gmail.com>
2026-08-05 00:51:25 +1000

    Can now change scenes to enter the level

--- PrimordiaGame/Assets/Scripts/SceneController.cs
+++ PrimordiaGame/Assets/Scripts/SceneController.cs
@@ -1,16 +1,8 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneController : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class SceneController : MonoBehaviour {
+    public void ChangeScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
     }
 }
